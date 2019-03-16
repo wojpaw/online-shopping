efff2fc0f8e5ad30f5a5550d1f08f80a5ee9ec2a
Pawel Wojtas <wojtasp@gmail.com>
2019-03-16 21:28:04 +0100

    Successfully loaded static resources

---
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -0,0 +1,42 @@
+package net.kzn.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = { "/", "/index", "/home" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+
+		mv.addObject("greeting", "Welcome to Spring MVC!");
+		return mv;
+	}
+
+	/*@RequestMapping(value ={"/test"})
+	public ModelAndView test(@RequestParam (value="greeting", required=false) String greeting){
+        if(greeting==null) {
+        	greeting= "hello there";
+        }
+		
+		ModelAndView mv = new ModelAndView("page");
+		
+		mv.addObject("greeting", greeting);
+		return mv;
+	}*/
+	@RequestMapping(value ={"/test/{greeting}"})
+	public ModelAndView test(@PathVariable (value="greeting") String greeting){
+        if(greeting==null) {
+        	greeting= "hello there";
+        }
+		
+		ModelAndView mv = new ModelAndView("page");
+		
+		mv.addObject("greeting", greeting);
+		return mv;
+	
+}
+}
